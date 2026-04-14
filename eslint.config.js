5f51909f4a78d4db34d8ecc09cb2f70269d48b0e
Ahmad Asad <mrahmadbutt338@gmail.com>
2026-04-14 20:12:55 +0500

    update project files

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,6 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
-// remove agar use nahi
-import { motion } from "framer-motion";
 
 export default defineConfig([
   globalIgnores(['dist']),
